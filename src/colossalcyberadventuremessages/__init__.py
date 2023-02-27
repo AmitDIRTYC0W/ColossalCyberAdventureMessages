f5c6af60c8002cd6bb6f835a389eb71ed48c45bc
Amit Goren <animalfarm@mailfence.com>
2023-02-27 21:36:44 +0200

    Turn this into a Python package

---
+++ src/colossalcyberadventuremessages/__init__.py
@@ -0,0 +1,18 @@
+"""
+Cap'n Proto schemas of the messages transferred between ColossalCyberAdventure's clients and
+servers.
+"""
+
+import capnp
+from pathlib import PurePath
+
+def load(filename: str):
+    return capnp.load(str(PurePath(__file__).parent.joinpath(filename)))
+
+identification = load('identification.capnp')
+
+del load
+del PurePath
+del capnp
+
+__version__ = "0.2.0"
